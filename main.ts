b4fd0644fcd44db2c29adaf71c7d37cf0bfc1e92
hchulvej <67361730+hchulvej@users.noreply.github.com>
2024-09-19 13:43:14 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Hello,")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("World!")
+})
+basic.showString("A or B")
+basic.forever(function () {
+	
+})
